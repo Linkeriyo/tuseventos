b6ec0b9d4ad9879af0c271ec888dccc71952954d
Linkeriyo <linkeriyo@gmail.com>
2022-06-16 18:48:51 +0200

    a

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from eventos.models import Article, ArticleType
+from eventos.models import Article, ArticleImage, ArticleType
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(ArticleType)
+admin.site.register(ArticleImage)
